407923a80899d23ffb31e2d3a1c23bc6aa8e3287
erich <eric.hine2214@gmail.com>
2024-05-29 19:20:57 -0400

    Expose inital stubbed APIs in preload process.

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,8 +1,27 @@
-import { contextBridge } from 'electron'
+import { contextBridge, ipcRenderer } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
 
 // Custom APIs for renderer
-const api = {}
+const api = {
+  getCollection: (collectinId) => ipcRenderer.invoke('get-collection', collectinId),
+  getCollections: () => ipcRenderer.invoke('get-collections'),
+  addCollection: (collectionData) => ipcRenderer.invoke('add-collection', collectionData),
+  deleteColection: (collectionId) => ipcRenderer.invoke('delete-collection', collectionId),
+  getTask: (taskId) => ipcRenderer.invoke('get-task', taskId),
+  getTaskByCollection: (taskId, collectionId) =>
+    ipcRenderer.invoke('get-task-by-collection', taskId, collectionId),
+  getTasksByCollection: (collectionId) =>
+    ipcRenderer.invoke('get-tasks-by-collection', collectionId),
+  addTask: (taskData) => ipcRenderer.invoke('add-task', taskData),
+  deleteTask: (taskId) => ipcRenderer.invoke('delete-task', taskId),
+  getEvent: (eventId) => ipcRenderer.invoke('get-event', eventId),
+  getEventByCollection: (eventId, collectionId) =>
+    ipcRenderer.invoke('get-event-by-collection', eventId, collectionId),
+  getEventsByCollection: (collectionId) =>
+    ipcRenderer.invoke('get-events-by-collection', collectionId),
+  addEvent: (eventData) => ipcRenderer.invoke('add-event', eventData),
+  deleteEvent: (eventId) => ipcRenderer.invoke('delete-event', eventId)
+}
 
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
