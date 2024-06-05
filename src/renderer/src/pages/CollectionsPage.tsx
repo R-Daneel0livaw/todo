98f975a35f0a574e0cfab5bd1608901dfc01b633
erich <eric.hine2214@gmail.com>
2024-06-05 18:01:59 -0400

    Start Collections Page as far as can go without adding DB.

---
+++ src/renderer/src/pages/CollectionsPage.tsx
@@ -0,0 +1,46 @@
+import { getCollections } from '@renderer/services/CollectionService'
+import { Collection } from '@shared/types'
+import { useEffect, useState } from 'react'
+
+function CollectionsPage() {
+  const [collections, setCollections] = useState<Collection[]>([])
+  const [expandedCollectionId, setExpandedCollectionId] = useState<number | null>(null)
+
+  useEffect(() => {
+    async function loadCollections() {
+      const collections = await getCollections()
+      setCollections(collections)
+    }
+    loadCollections()
+  }, [])
+
+  const handleExpand = (collectionId: number) => {
+    setExpandedCollectionId(expandedCollectionId === collectionId ? null : collectionId)
+  }
+
+  return (
+    <div>
+      <h1>Collection List</h1>
+      <ul>
+        {collections.map((collection) => (
+          <li key={collection.id}>
+            <div onClick={() => handleExpand(collection.id)}>
+              <h2>{collection.title}</h2>
+              <p>{collection.description}</p>
+            </div>
+            {expandedCollectionId === collection.id && (
+              <div>
+                <p>Type: {collection.type}</p>
+                <p>Sub-Type: {collection.subType}</p>
+                <p>Created At: {collection.createDate.toLocaleString()}</p>
+                <p>Created At: {collection.startDate?.toLocaleString()}</p>
+              </div>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default CollectionsPage
