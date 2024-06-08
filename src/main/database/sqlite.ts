35c1d6972f125a7a83b37216c30e3e9e4a9f8d9d
erich <eric.hine2214@gmail.com>
2024-06-08 14:36:11 -0400

    Create TABLE sql added for task, event and collection.

--- src/main/database/sqlite.ts
+++ src/main/database/sqlite.ts
@@ -8,8 +8,12 @@ db.exec(`
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT,
     description TEXT,
-    isCompleted BOOLEAN,
-    createdAt TEXT,
+    topic TEXT,
+    status TEXT,
+    createDate TEXT,
+    startDate TEXT,
+    endDate TEXT,
+    canceledDate TEXT,
     topic TEXT
   );
 
@@ -18,21 +22,33 @@ db.exec(`
     title TEXT,
     description TEXT,
     location TEXT,
-    date TEXT,
-    createdAt TEXT
+    status TEXT,
+    createDate TEXT,
+    startDate TEXT,
+    endDate TEXT,
+    canceledDate TEXT
   );
 
   CREATE TABLE IF NOT EXISTS collections (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT
+    title TEXT,
+    description TEXT,
+    type TEXT,
+    subType TEXT,
+    createDate TEXT,
+    startDate TEXT,
+    endDate TEXT,
+    canceledDate TEXT
   );
 
   CREATE TABLE IF NOT EXISTS collectionItems (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    collectionId INTEGER,
+    collectionId INTEGER NOT NULL,
     itemId INTEGER,
-    itemType TEXT,
-    FOREIGN KEY (collectionId) REFERENCES collections (id)
+    itemType TEXT NOT NULL CHECK (itemType IN ('Task', 'Event', 'Collection')),
+    FOREIGN KEY (collectionId) REFERENCES collections (id),
+    FOREIGN KEY (itemId) REFERENCES tasks(id) ON DELETE CASCADE,
+    FOREIGN KEY (itemId) REFERENCES events(id) ON DELETE CASCADE
   );
 `)
 
