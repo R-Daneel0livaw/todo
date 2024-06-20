27d969381d8b14cabebe4076a0c145f0ea43bd2e
erich <eric.hine2214@gmail.com>
2024-06-20 18:51:27 -0400

    Add base styling for Collections Page and Collection View.

--- src/renderer/src/components/CollectionView/CollectionView.tsx
+++ src/renderer/src/components/CollectionView/CollectionView.tsx
@@ -11,13 +11,15 @@ function CollectionView({ collection, isExpanded, onExpand }: CollectionViewProp
   return (
     <div>
       <div className={styles.collectionViewContainer} onClick={() => onExpand(collection.id)}>
-        <h2>{collection.title}</h2>
+        <h2>
+          <span className={styles.collectionViewType}>[{collection.type.charAt(0)}]</span>
+          <span className={styles.collectionViewType}>[{collection.subType.charAt(0)}]</span>
+          {collection.title}
+        </h2>
         <p>{collection.description}</p>
       </div>
       {isExpanded && (
         <div className={styles.collectionViewContainer}>
-          <p>Type: {collection.type}</p>
-          <p>Sub-Type: {collection.subType}</p>
           <p>Created At: {collection.createDate.toLocaleString()}</p>
           {collection.startDate && <p>Started At: {collection.startDate.toLocaleString()}</p>}
           {collection.endDate && <p>Ended At: {collection.endDate.toLocaleString()}</p>}
