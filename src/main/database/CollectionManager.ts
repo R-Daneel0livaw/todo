f6f7ab9de80c3d720aeda9b3b30306482526ef24
erich <eric.hine2214@gmail.com>
2024-06-30 09:52:52 -0400

    Add new collection manager query for add and retrieve in same call.

--- src/main/database/CollectionManager.ts
+++ src/main/database/CollectionManager.ts
@@ -1,6 +1,10 @@
 import { Collection } from '@shared/types'
 import db from './sqlite'
 
+  interface LastInsertRowId {
+    id: number
+}
+
 export function getCollection(collectionId: number): Collection {
   const stmt = db.prepare('SELECT * FROM collections WHERE id = ?')
   const collection: Collection = stmt.get(collectionId) as Collection
@@ -26,6 +30,13 @@ export function addCollection(collectionData: Collection) {
     collectionData.createDate?.toISOString(),
     collectionData.startDate?.toISOString()
   )
+}
+
+export function addAndRetrieveCollection(collectionData: Collection) {
+  addCollection(collectionData)
+  const result = db.prepare('SELECT last_insert_rowid() as id').get() as LastInsertRowId
+  const newCollection = getCollection(result.id)
+  return newCollection
 }
 
 export function deleteCollection(collectionId: number) {
